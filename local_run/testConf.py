0952f1393f904486a1945c228369cd367e5d85c1
ktaniguc <guchiwo7@gmail.com>
2020-11-12 11:18:56 +0900

    update DEBUG stream

---
+++ local_run/testConf.py
@@ -0,0 +1,10 @@
+
+from TrigL2MuonSA.TrigL2MuonSAConf import *
+from TrigL2MuonSA.TrigL2MuonSAConfig import TrigL2MuonSAMTConfig
+#from TrigmuComb.TrigmuCombConf import *
+#from TrigmuComb.TrigmuCombConfig import TrigmuCombConfig
+from TrigMuonHypoMT.TrigMuonHypoMTConf import *
+from TrigMuonHypoMT.TrigMuonHypoMTConfig import TrigMufastHypoConfig
+
+TrigMufastHypoConfig.OutputLevel = DEBUG
+TrigL2MuonSAMTConfig.OutputLevel = DEBUG
